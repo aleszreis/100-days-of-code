8b97fdaf81cbc22031d707001efa784b43bfeeec
Alessandra Reis <79385226+aleszreis@users.noreply.github.com>
2021-08-24 20:52:54 -0300

    Add files via upload

--- Rainy-day Warning/main.py
+++ Rainy-day Warning/main.py
@@ -1,1 +1,43 @@
+import requests
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+import os
 
+
+OWM_ENDPOINT = "OPEN WEATHER MAP ENDPOINT"
+api_key = "API KEY"
+account_sid = "ACC SID"
+auth_token = "AUTH TOKEN"
+
+parameters = {
+    "lat": -22.906847,
+    "lon": -43.172897,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(url=OWM_ENDPOINT, params=parameters)
+response.raise_for_status()
+
+data = response.json()
+
+
+data_slice = data["hourly"][:16]
+will_rain = False
+for hour in data_slice:
+    condition_code = hour["weather"][0]["id"]
+    if int(condition_code) < 600:
+        will_rain = True
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {"https": os.environ["https_proxy"]}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="Vai chover hoje!",
+        from_="+twilioPhoneNumber",
+        to="+userPhoneNumber"
+    )
+
+    print(message.status)
