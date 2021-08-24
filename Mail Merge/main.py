47397b1b96ff5bb696acfe4d3f0c21e9d3302b71
Alessandra Reis <79385226+aleszreis@users.noreply.github.com>
2021-08-24 20:26:57 -0300

    Add files via upload

--- Mail Merge/main.py
+++ Mail Merge/main.py
@@ -1,1 +1,15 @@
+PLACEHOLDER = "[name]"
 
+
+with open("./Input/Names/invited_names.txt") as file:
+    nomes = file.readlines()
+
+with open("./Input/Letters/starting_letter.docx", encoding='UTF-8') as file:
+    content = file.read()
+
+for nome in nomes:
+    nome_sem_espaco = nome.strip()
+    nova_carta = content.replace(PLACEHOLDER, nome_sem_espaco)
+
+    with open(f"./Output/ReadyToSend/letter_for_{nome_sem_espaco}.txt", mode="w") as completed_letter:
+        completed_letter.write(nova_carta)
