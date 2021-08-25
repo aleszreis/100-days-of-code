d67ecf3ffd945ae65d73bb4eb628c6a216aafaca
Alessandra Reis <79385226+aleszreis@users.noreply.github.com>
2021-08-24 21:02:41 -0300

    Add files via upload

--- Virtual Bookshelf/main.py
+++ Virtual Bookshelf/main.py
@@ -1,1 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  ## Optional
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+db.create_all()
+
+
+@app.route('/')
+def home():
+    all_books = db.session.query(Book).all()
+    return render_template('index.html', book_list=all_books)
+
+
+@app.route('/delete')
+def delete():
+    db.session.delete(Book.query.get(request.args.get('book_id')))
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        new_book = Book(title=request.form['title'], author=request.form['author'], rating=request.form['rating'])
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add.html')
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method == 'POST':
+        book_to_edit = Book.query.get(request.form["id"])
+        book_to_edit.rating = request.form["new_rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book = Book.query.get(request.args.get('book_id'))
+    return render_template('edit.html', book=book)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
